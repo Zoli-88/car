1162aca1a81b493baeeed1a405d0d793b88b1e46
Zoltan H <zoltan.h@wavemyth.com>
2024-03-06 12:14:48 +0200

    completed car scale functionality with a range input

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// ===== Variables =====
+// DOM Elements
 const $rangeInput = document.querySelector('input[id="scale"]');
+const $car = document.querySelector('.car-svg');
 
-$rangeInput.addEventListener('change', function() {
-  console.log(this);
-})
+// Initial scale of the car (default)
+const initialScaleValue = 1;
+
+// Initialize car scale with the default value
+scaleCar();
+
+// ===== Event Listeners =====
+$rangeInput.addEventListener('input', scaleCar);
+
+// ===== Functions =====
+function scaleCar() {
+  const scaleValue = Number(this.value) || initialScaleValue;
+  $car.style.transform = `scale(${scaleValue})`;
+}
+
